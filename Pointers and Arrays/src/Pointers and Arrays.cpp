f666cbc8341bdb2e03706fb90587297f26c85ff2
JingWang <jingwang9302@gmail.com>
2019-02-18 11:35:18 -0800

    array using pointerr

--- Pointers and Arrays/src/Pointers and Arrays.cpp
+++ Pointers and Arrays/src/Pointers and Arrays.cpp
@@ -1,9 +1,9 @@
 //============================================================================
 // Name        : Pointers.cpp
 // Author      : Zoe
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+//Loop through an array using a pointer, with array element reference syntax
+//Loop through an array by implementing a pointer
+//Loop through an array, stopping by comparing two pointers
 //============================================================================
 
 #include <iostream>
@@ -11,19 +11,36 @@ using namespace std;
 
 int main() {
 	string texts[]={"one","two","three"};
-	for(int i=0;i<sizeof(texts)/sizeof(string);i++){
-		cout<< texts[i] << "  ";
-	}
 	string *pTexts = texts;
-	for(int i=0; i<sizeof(texts)/sizeof(string);i++){
-		cout<< pTexts[i]<< "  ";
-	}
-//pointer in the array
-	for(int i=0;i< sizeof(texts)/sizeof(string);i++,pTexts++){
-		cout<< *pTexts<< "  ";
 
+	//array no pointer
+	for(int i=0;i< sizeof(texts)/sizeof(string);i++){
+		cout<< texts[i]<<" ";
 	}
-	cout<< 90/60<<endl;
+	cout<<endl;
+
+	//pointer array
+	for(int i=0;i< sizeof(texts)/sizeof(string);i++){
+		cout<< pTexts[i]<<" ";
+	}
+	cout<< endl;
+
+	//pointer array
+	for(int i=0;i< sizeof(texts)/sizeof(string);i++,*pTexts++){
+		cout<< *pTexts<< " ";
+	}
+	cout<< endl;
+
+	//pointer array
+	string* pElement = &texts[0];
+	string* pEnd = &texts[2];
+	while(true){
+		cout<< *pElement<< " ";
+		if(pElement == pEnd){
+			break;
+		}
+		pElement++;
+	}
 
 	return 0;
 }
